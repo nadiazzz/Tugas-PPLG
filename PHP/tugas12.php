e8852a4bd64f11c35b60a572c9bce4a11e20de46
nadiazzz <nadmirzyworks@gmail.com>
2025-05-15 15:35:16 +0800

    Tugas 12, Array

---
+++ PHP/tugas12.php
@@ -0,0 +1,21 @@
+<?php
+$Nama_Anggota = [
+    "Akifah Zahrani Arafat",
+    "Al Qumaerah",
+    "Althaf Azzah Zahra",
+    "Andi Al Firqahtunnajiyah Ahsyar",
+    "Andi Mufrodatul Fanzila",
+    "Fahrah Furaidah Sahrir",
+    "Fauziyyah",
+    "Jilan Fathiyyah",
+    "Nadia Mirzayanti Hasbi",
+    "Rabiatul Adawiyah",
+    "Alifah Izzah Rahmadini",
+    "Muftihaturrahmah",
+    "Auliyah Nastity Khayla Armada",
+    "Putri Kinanti Almagfirah"
+];
+echo "Nama Anggota Kelas 10 B: </br>";
+    foreach ($Nama_Anggota as $Daftar_Anggota){
+        echo "- " . $Daftar_Anggota . "</br>";
+    }
